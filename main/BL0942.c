4f5f9cbe9290ab11f0ead2cf6f9c217024d94ab6
JovisDreams <zhoudreamstk@foxmail.com>
2025-05-14 19:50:45 +0800

    将功率改为用BL0942内的寄存器的数据计算

--- main/BL0942.c
+++ main/BL0942.c
@@ -16,16 +16,20 @@
 
 #define TAG "BL0942.c"
 
-float BL0942_POWER = 0;
+float BL0942_POWER = 0; //全局变量，BL0942检测到的有功功率
+int POWER_THRESH = 0; //全局变量，用户设置的功率限制
+int POWER_ACCUMULATION = 0; //全局变量，已用电量
 
+//BL0942 IC初始化
 void UART_BL0942_INST()
 {
-    ESP_ERROR_CHECK(uart_driver_install(UART_BL0942_NUM, BUF_SIZE, BUF_SIZE, 10, &uart1_event_queue, 0));
+    ESP_ERROR_CHECK(uart_driver_install(UART_BL0942_NUM, BUF_SIZE, BUF_SIZE, 10, NULL, 0));
     ESP_ERROR_CHECK(uart_param_config(UART_BL0942_NUM, &uart_config_BL0942));
     ESP_ERROR_CHECK(uart_set_pin(UART_BL0942_NUM, UART_BL0942_TX, UART_BL0942_RX, -1, -1));
     ESP_LOGI(TAG, "UART_BL0942 has been installed.\n");
 }
 
+//BL0942电量计量任务
 void BL0942_READ_TASK()
 {
     char cmd = '\0';
@@ -34,6 +38,7 @@ void BL0942_READ_TASK()
     uint32_t p_rms_raw = 0;
     float voltage_AC_IN = 0;
     float current_OUT = 0;
+    float power_LOAD = 0;
 
     while(1)
     {
@@ -52,12 +57,12 @@ void BL0942_READ_TASK()
         char response[50] = "\0";
         int len = uart_read_bytes(UART_BL0942_NUM, response, BUF_SIZE - 1, 10);
 
-        ESP_LOGI(TAG, "Response length %d: ",len);
-        for(int i=0; i<len; i++)
-        {
-            printf("%02X ",response[i]); //'X'是指输出十六进制，'2'是指变成两位，'0'是指用0填充
-        }
-        printf("\n");
+        // ESP_LOGI(TAG, "Response length %d: ",len);
+        // for(int i=0; i<len; i++)
+        // {
+        //     printf("%02X ",response[i]); //'X'是指输出十六进制，'2'是指变成两位，'0'是指用0填充
+        // }
+        // printf("\n");
 
         if(len != 23)
         {
@@ -66,18 +71,19 @@ void BL0942_READ_TASK()
             continue;
         }
 
-        v_rms_raw= (response[6] << 16) | (response[5] << 8) | response[4]; 
+        v_rms_raw = (response[6] << 16) | (response[5] << 8) | response[4]; 
         voltage_AC_IN = (v_rms_raw * 1.218*( 390*5 + 0.51 )) / (73989*0.51*1000);//3.824=（R1+R2）/R1*1000
-        a_rms_raw= (response[3] << 16) | (response[2] << 8) | response[1]; 
+        a_rms_raw = (response[3] << 16) | (response[2] << 8) | response[1]; 
         current_OUT =  (a_rms_raw * 1.218 ) / (305978/(0.001*1000));
+        p_rms_raw = (response[12] << 16) | (response[11] << 8) | response[10];
+        power_LOAD = (p_rms_raw * 1.218 * 1.218 * (390*5 + 0.51) ) / (3537 * 1 * 0.51 * 1000);
 
-        voltage_AC_IN = roundf(voltage_AC_IN*100)/100;
-        current_OUT = roundf(current_OUT*100)/100;
-        BL0942_POWER = voltage_AC_IN * current_OUT;
-        ESP_LOGI(TAG, "Voltage: %0.1fV, Current: %0.1fA, Power: %0.1fW",voltage_AC_IN, current_OUT, BL0942_POWER);
+        BL0942_POWER = roundf(power_LOAD*10)/10;
+        
+        //ESP_LOGI(TAG, "Voltage: %0.1fV, Current: %0.1fA, Power: %0.1fW",voltage_AC_IN, current_OUT, BL0942_POWER);
         
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(500));
 
     } 
 
